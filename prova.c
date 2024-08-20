f6758a8e0318bda4a608e824e10ae5b2daf44516
Edoardo <edoberri97@gmail.com>
2024-08-20 09:39:27 -0400

    Esercizi 20 08

---
+++ prova.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+* Questo e' un programma in C
+*/
+
+int main ()
+{
+  printf("**************\n");
+  printf("hello world\n ");
+  printf("***************\n");
+  return 0;
+}
